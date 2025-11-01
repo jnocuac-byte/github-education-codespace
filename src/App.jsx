e369d033a22714f2e1c0360fa715cedac44cb784
Juan Esteban <jnocuac@ucentral.edu.co>
2025-11-01 17:35:24 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Lawm Codespace",
+  title: "Programmer | Developer | Coder",
+  email: "jnocuac@ucentral.edu.co",
+  gitHub: "jnocuac-byte",
+  instagram: "jnocuac",
+  linkedIn: "jnocuac",
   medium: "",
-  twitter: "microsoft",
+  twitter: "jnocuac",
   youTube: "Code",
 };
 
